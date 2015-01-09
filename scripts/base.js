d80496befb319f94ca8041c904ace463be94323e
Znegl <mail@thomasgrinderslev.dk>
2015-01-09 21:46:07 +0100

    Added missing 'w'

--- scripts/base.js
+++ scripts/base.js
@@ -5,7 +5,7 @@
 	var alphabetOutputCells = document.getElementById('alphabet-output-cells');
 
 	var allCiphers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-	var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', 'æ', 'ø', 'å'];
+	var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å'];
 
 	/**
 	 * Updates the cipher reference to match the current ciphers.
