91b221325530b6eec141339e3999d462a171ab7f
Znegl <mail@thomasgrinderslev.dk>
2015-01-09 23:08:23 +0100

    Added basic coordinate converter

--- scripts/base.js
+++ scripts/base.js
@@ -3,9 +3,16 @@
 	var cipherOutput = document.getElementById('cipher-output');
 	var alphabetOutputHeaders = document.getElementById('alphabet-output-headers');
 	var alphabetOutputCells = document.getElementById('alphabet-output-cells');
+	var coordinateN1 = document.getElementById('coordinate-n-1');
+	var coordinateN2 = document.getElementById('coordinate-n-2');
+	var coordinateNOutput = document.getElementById('coordinate-n-output');
+	var coordinateE1 = document.getElementById('coordinate-e-1');
+	var coordinateE2 = document.getElementById('coordinate-e-2');
+	var coordinateEOutput = document.getElementById('coordinate-e-output');
 
 	var allCiphers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 	var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å'];
+	var currentCiphers = allCiphers;
 
 	/**
 	 * Updates the cipher reference to match the current ciphers.
@@ -41,17 +48,60 @@
 		var headers = '';
 		var cells = '';
 		alphabet.forEach(function(letter, index) {
-			var cipherIndex = index % ciphers.length;
-			var cipher = ciphers[cipherIndex];
 			headers += '<th>' + letter + '</th>';
-			cells += '<td>' + cipher + '</td>';
+			cells += '<td>' + getCipherFromLetter(letter, index) + '</td>';
 		});
 
 		alphabetOutputHeaders.innerHTML = '<tr>' + headers + '</tr>';
 		alphabetOutputCells.innerHTML = '<tr>' + cells + '</tr>';
 	}
 
-	cipherInput.addEventListener('input', function() {
+	/**
+	 * Returns the matching cipher for the given letter.
+	 * If no cipher is found the given letter is returned.
+	 * @param {string} letter
+	 * @param {number=} letterIndex
+	 * @returns {number|string}
+	 */
+	function getCipherFromLetter(letter, letterIndex) {
+		if (letterIndex === undefined) {
+			letterIndex = alphabet.indexOf(letter);
+		}
+		if (letterIndex == -1) {
+			return letter;
+		}
+		var cipherIndex = letterIndex % currentCiphers.length;
+		return currentCiphers[cipherIndex];
+	}
+
+	/**
+	 * Updates the converted coordinate to match the current cipher positions.
+	 */
+	function updateCoordinate() {
+		// North
+		var nString = '';
+		var coordinateNArray = coordinateN1.value.split('');
+		coordinateNArray.push('°');
+		coordinateNArray.push(' ');
+		coordinateNArray.push.apply(coordinateNArray, coordinateN2.value.split(''));
+		coordinateNArray.forEach(function(letter) {
+			nString += getCipherFromLetter(letter);
+		});
+		coordinateNOutput.textContent = 'N ' + nString;
+
+		// East
+		var eString = '';
+		var coordinateEArray = coordinateE1.value.split('');
+		coordinateEArray.push('°');
+		coordinateEArray.push(' ');
+		coordinateEArray.push.apply(coordinateEArray, coordinateE2.value.split(''));
+		coordinateEArray.forEach(function(letter) {
+			eString += getCipherFromLetter(letter);
+		});
+		coordinateEOutput.textContent = 'E ' + eString;
+	}
+
+	document.body.addEventListener('input', function() {
 		// Check for validation errors if supported by the browser
 		if (cipherInput.checkValidity && !cipherInput.checkValidity()) {
 			updateCipherReference(null);
@@ -81,12 +131,15 @@
 			}
 		});
 
+		currentCiphers = ciphers;
+
 		// Print ciphers to the user
 		updateCipherReference(ciphers);
 		updateAlphabeticReference(ciphers);
+		updateCoordinate();
 	});
 
 	updateCipherReference(allCiphers);
 	updateAlphabeticReference(allCiphers);
-
+	updateCoordinate();
 })();
